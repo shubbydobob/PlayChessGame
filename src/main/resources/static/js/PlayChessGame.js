5ce307f855f6963823271c5ea8204bf1019f5555
Shubby <shubbydobob@gmail.com>
2025-01-22 16:10:31 +0900

    -> 비회원 사용자가 방 생성 시도할 때 로그인 / 계정 생성할 수 있도록 유도
    - Controller와 Js 수정

--- src/main/resources/static/js/PlayChessGame.js
+++ src/main/resources/static/js/PlayChessGame.js
@@ -1,20 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Thymeleaf에서 로그인 여부 가져오기
-    const isLoggedIn  = false;
     const createRoomBtn = document.querySelector("#createRoomBtn");
 
-    if(createRoomBtn) {
-      createRoomBtn.addEventListener("click", function(event){
-        if (!isLoggedIn) {
-         event.preventDefault();
-         alert("방문해주셔서 감사합니다! 계정 생성 후 로그인 먼저 해주세요");
-      } else {
-          window.location.href="/Room";
-      }
-   });
-  }
-   const notLoggedIn = false;
-   if(notLoggedIn) {
-     alert("로그인 먼저 해주세요");
-   }
+    if (createRoomBtn) {
+        createRoomBtn.addEventListener("click", function(event) {
+            if (!isLoggedIn) {
+                event.preventDefault();
+                alert("방문해주셔서 감사합니다! 계정 생성 후 로그인 먼저 해주세요");
+            } else {
+                window.location.href = "/Room";
+            }
+        });
+    }
 });
